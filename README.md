8a0e9ddec4ec0a4dba30fd65102746ca09565481
mpcs53013 <jgang@chicagobooth.edu>
2017-10-15 22:20:40 -0500

    commit of part 3-3

--- README.md
+++ README.md
@@ -1,9 +1,24 @@
 3-1:
 
 3-2:
-The jgang32 folder contains the java package minus the jar files.
+The jgang32 folder contains the Java package minus the jar files.
+
+jgang32/src/main/thrift/WeatherAnalytics.thrift contains the thrift
+file for the data.
+
+jgang32/src/main/java/edu/uchicago/mpcs53013/DemoSerialization.java
+contains my updated code from the example that deals with the GSOD
+data instead.
 
 thrift32.out in the main directory is a thrift output example using
 data from 710730-99999-1990.op, which is also in the main directory.
 
 3-3:
+The jgang folder contains the Java package minus the jar files.
+
+jgang/src/main/java/edu/uchicago/mpcs53103/jgang/WordCount.java
+contains my updated code from the example that now splits words
+into trigraphs before counting them.
+
+part-r-00000 in the main directory is an output example of a run on
+the following text: "some message blah hello world world"
---
+++ jgang/src/main/java/edu/uchicago/mpcs53103/jgang/WordCount.java
@@ -0,0 +1,74 @@
+package edu.uchicago.mpcs53103.jgang;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+// Taken from https://hadoop.apache.org/docs/current/hadoop-mapreduce-client/hadoop-mapreduce-client-core/MapReduceTutorial.html#Source_Code
+public class WordCount {
+
+  public static class TokenizerMapper
+       extends Mapper<Object, Text, Text, IntWritable>{
+
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+
+    public void map(Object key, Text value, Context context
+                    ) throws IOException, InterruptedException {
+      StringTokenizer itr = new StringTokenizer(value.toString());
+      while (itr.hasMoreTokens()) {
+        word.set(itr.nextToken());
+        
+        String wordString = word.toString();
+        for (int i = 0; i < wordString.length(); i++)
+        {
+        	if ((i + 2) >= wordString.length())
+        	{
+        		break;
+        	}
+        	String trigraph = wordString.substring(i, i+3);
+        	Text tri = new Text(trigraph);
+        	context.write(tri, one);
+        }              
+      }
+    }
+  }
+
+  public static class IntSumReducer
+       extends Reducer<Text,IntWritable,Text,IntWritable> {
+    private IntWritable result = new IntWritable();
+
+    public void reduce(Text key, Iterable<IntWritable> values,
+                       Context context
+                       ) throws IOException, InterruptedException {
+      int sum = 0;
+      for (IntWritable val : values) {
+        sum += val.get();
+      }
+      result.set(sum);
+      context.write(key, result);
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+    conf.addResource(new Path("/home/mpcs53013/hadoop/etc/hadoop/core-site.xml"));
+    Job job = Job.getInstance(conf, "word count");
+    job.setJarByClass(WordCount.class);
+    job.setMapperClass(TokenizerMapper.class);
+    job.setReducerClass(IntSumReducer.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
+}
